235dd97db1ddf73e560bc1cf41468283b8bc88b2
Zablon Okibeni <39940048+Zabinyyo@users.noreply.github.com>
2022-03-15 02:27:53 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,18 @@
-j
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int s;
+s = 122;
+while (s >= 97)
+{
+putchar(s);
+s--;
+}
+putchar('\n');
+return (0);
+}
