83eb2295b4fe5ee9b795d4947ef3999729528690
Zablon Okibeni <39940048+Zabinyyo@users.noreply.github.com>
2022-03-15 02:25:37 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,17 @@
-j
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int r;
+
+for (r = 0; r <= 9; r++)
+{
+printf("%d", r);
+}
+printf("\n");
+return (0);
+}
