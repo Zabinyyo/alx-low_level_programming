17cd0043550aff571cdcc30bef33673223346fe3
Zablon Okibeni <39940048+Zabinyyo@users.noreply.github.com>
2022-03-15 23:47:53 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,13 @@
-J
+#include "main.h"
+
+/**
+ * _islower - chack lowercase characters
+ * @c: the character to print
+ *
+ * Return: On success 1 else 0
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
