ff9bdd95986403636b0b6c68427d1d8ced85746e
Zablon Okibeni <39940048+Zabinyyo@users.noreply.github.com>
2022-03-16 00:41:12 +0300

    Update 10-main.c

--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,21 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - Entry point
- * Write a program that prints _putchar, followed by a new line.
+ * main - check the code
  *
- * Return: always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
 }
