a7dbb792f704c02fe37eb5ebf69b9fb2c685269c
Zablon Okibeni <39940048+Zabinyyo@users.noreply.github.com>
2022-03-22 00:24:35 +0300

    Update 2-main.c

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -1,1 +1,18 @@
-V1
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+    int len;
+
+    str = "My first strlen!";
+    len = _strlen(str);
+    printf("%d\n", len);
+    return (0);
+}
