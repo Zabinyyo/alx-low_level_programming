2104c09b03fcc6736525e7f431d716d62304e3ab
Zablon Okibeni <39940048+Zabinyyo@users.noreply.github.com>
2022-03-22 00:36:52 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,21 @@
-V1
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+*/
+
+void puts_half(char *str)
+{
+int len = 0;
+for (len = 0; str[len] != '\0'; len++)
+;
+
+for (int i = (len + 1) / 2; str[i] != '\0'; i++)
+{
+	_putchar(str[i]);
+}
+_putchar('\n');
+}
