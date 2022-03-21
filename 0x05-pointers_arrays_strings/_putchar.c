7d672bd8808a8a164307f4b65168e3161d70e334
Zablon Okibeni <39940048+Zabinyyo@users.noreply.github.com>
2022-03-22 00:42:55 +0300

    Update _putchar.c

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,1 +1,13 @@
-V1
+#include <unstd.h>
+
+/**
+ *_putchar - writes the character c to stdout
+ *@c: the main character to print
+ *Return: Always 1 (Success)
+ *on error, -1 is returned, and errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+  return (write(1, &c, 1));
+}
