66564617f4e8979b8f1658b65c4007285bd6fbf7
Zablon Okibeni <39940048+Zabinyyo@users.noreply.github.com>
2022-03-30 22:29:00 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,44 +1,30 @@
 #include "main.h"
 
-int tmp_prime(int n, int i);
-
 /**
- * divisors - number is prime?
- * @n: integer params
- * @m: integer params
- * Return: boolean
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
 
-int divisors(int n, int m)
+int wildcmp(char *s1, char *s2)
 {
-	if (m % n == 0)
+	if (*s1 == '\0')
 	{
-		return (0);
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
 	}
-	else if (m / 2 > n)
-	{
-		return (divisors(n + 2, m));
-	}
-	else
-	{
-		return (1);
-	}
-}
 
-/**
- * is_prime_number - prime
- * @n: integer params
- * Return: recursion
- */
-
-int is_prime_number(int n)
-{
-	if ((!(n % 2) && n != 2) || n < 2)
+	if (*s2 == '*')
 	{
-		return (0);
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
-	else
+	else if (*s1 == *s2)
 	{
-		return (divisors(3, n));
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
+	return (0);
 }
