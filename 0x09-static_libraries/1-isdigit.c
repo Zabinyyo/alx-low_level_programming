9668d60776d76025ff84d692a28f7ba69ce3ede7
Zablon Okibeni <39940048+Zabinyyo@users.noreply.github.com>
2022-04-02 03:15:22 +0300

    Update 1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,1 +1,16 @@
-v1
+#include "main.h"
+
+/**
+ * _isdigit - check the code for Holberton School students.
+ *
+ * @c: is an integer param
+ *
+ * Return: Always 0.
+ */
+
+
+int _isdigit(int c)
+{
+
+	return (c >= 48 && c <= 57);
+}
