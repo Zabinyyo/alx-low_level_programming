4b89db21078e26ac79598671ef9f4696989299fe
Zablon Okibeni <39940048+Zabinyyo@users.noreply.github.com>
2022-04-05 00:33:22 +0300

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,21 +1,13 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
-char *_strdup(char *str);
-
-char **strtow(char *str);
-
-char *argstostr(int ac, char **av);
-
-char *str_concat(char *s1, char *s2);
-
-void free_grid(int **grid, int height);
-
-int **alloc_grid(int width, int height);
+/**
+ * declaration of prototypes
+ */
+int _putchar(char c);
 
 char *create_array(unsigned int size, char c);
+
+char *_strdup(char *str);
 
 #endif
