41f47d91ab789a9f3fa3c061a6490afa0cfd8745
Zablon Okibeni <39940048+Zabinyyo@users.noreply.github.com>
2022-04-05 00:26:02 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,65 +1,48 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _strlen - length of a string
- * @s: input char
- * Return: length of a string
- */
-
-int _strlen(char *s)
-{
-	int l = 0;
-
-	while (*s != '\0')
-	{
-		s++;
-		l++;
-	}
-	return (l);
-}
-
-/**
- * argstostr - concat
- * @ac: count
- * @av: vector
- * Return: string
+ * *argstostr - concatenates all arguments of
+ * your program
+ * @ac: int type
+ * @av: char pointer to a pointer
+ * Return: pointer to a new string,
+ * or NULL if it fails
  */
 
 char *argstostr(int ac, char **av)
 {
-	int i, j, k;
-	int len, R = 0;
-	char *p;
+	int a, b, c = 0, len = 0;
+	char *z;
 
-	if (!ac || !av)
-	{
-		return (NULL);
-	}
-	R = 0;
-
-	for (i = 0; i < ac; i++)
-	{
-		len = _strlen(av[i]) + 1;
-		R += len;
-	}
-	p = malloc(sizeof(char) * R + 1);
-
-	if (!p)
+	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; i < ac; i++)
+	for (a = 0; a < ac; a++)
 	{
-		len = _strlen(av[i]);
+		for (b = 0; av[a][b]; b++)
+			len++;
+	}
+	len += ac;
 
-		for (j = 0; j < len; j++, k++)
+	z = malloc(sizeof(char) * len + 1);
+	if (z == NULL)
+		return (NULL);
+
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b]; b++)
 		{
-			p[k] = av[i][j];
+			z[c] = av[a][b];
+			c++;
 		}
-		p[k++] = '\n';
+		if (z[c] == '\0')
+		{
+			z[c++] = '\n';
+		}
 	}
-	p[k] = '\0';
-	return (p);
+	return (z);
 }
