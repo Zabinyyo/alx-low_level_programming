064195d702204bf7b6ece1a1e8d8cf54457a006d
Zablon Okibeni <39940048+Zabinyyo@users.noreply.github.com>
2022-04-05 00:05:55 +0300

    Update 0-create_array.c

--- 0x0B-mallac_free/0-create_array.c
+++ 0x0B-mallac_free/0-create_array.c
@@ -1,25 +1,36 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * create_array - create array
- * @size: size
- * @c: constant
- * Return: char pointer
+ * *create_array - creates an array of chars,
+ * and initializes it with a specific char
+ * @size: size of the array
+ * @c: char to initialize
+ * Return: 0 if success, NULL if size is equal to 0
  */
+
 char *create_array(unsigned int size, char c)
 {
-	char *p;
-	unsigned int i;
+	unsigned int a;
+	char *b;
 
+	a = 0;
 	if (size == 0)
-	return (0);
-	p = malloc((size) * sizeof(char));
-	if (p == '\0')
-	return (0);
-	for (i = 0; i < size; i++)
 	{
-		p[i] = c;
+		return (NULL);
 	}
-return (p);
+
+	b = malloc(sizeof(*b) * size);
+
+	if (b == NULL)
+		return (NULL);
+
+	while (a < size)
+	{
+		*(b + a) = c;
+		a++;
+	}
+
+	return (b);
 }
