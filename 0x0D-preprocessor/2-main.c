8a0b653ed1815d9dcf495ac5d15aebd1ab1575d3
Zablon Okibeni <39940048+Zabinyyo@users.noreply.github.com>
2022-04-12 00:54:29 +0300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file.
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
