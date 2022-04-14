b1d32eb02fe92cdaca6f086bba125b0d51376706
Zablon Okibeni <39940048+Zabinyyo@users.noreply.github.com>
2022-04-14 23:20:19 +0300

    Update 2-print_string.c

--- 0x10-variadic_functions/2-print_string.c
+++ 0x10-variadic_functions/2-print_string.c
@@ -1,51 +1,43 @@
+/*
+ * File: 2-print_strings.c
+ * Auth: Tolulope Fakunle
+ */
+
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
 
 /**
-* print_all - print char, integer, float and string
-* @format: format
-*/
-void print_all(const char * const format, ...)
+ * print_strings - Prints strings, followed by a new line.
+ * @separator: The string to be printed between strings.
+ * @n: The number of strings passed to the function.
+ * @...: A variable number of strings to be printed.
+ *
+ * Description: If separator is NULL, it is not printed.
+ *              If one of the strings if NULL, (nil) is printed instead.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list list;
-	unsigned int j = 0, start = 0;
-	char *p;
+	va_list strings;
+	char *str;
+	unsigned int index;
 
-	va_start(list, format);
-	while (format && format[j] != '\0')
+	va_start(strings, n);
+
+	for (index = 0; index < n; index++)
 	{
-		switch (format[j])
-		{ case 'c':
-			switch (start)
-			{ case 1: printf(", "); }
-			start = 1;
-			printf("%c", va_arg(list, int));
-			break;
-			case 'i':
-			switch (start)
-			{ case 1: printf(", "); }
-			start = 1;
-			printf("%i", va_arg(list, int));
-			break;
-		case 'f':
-			switch (start)
-			{ case 1: printf(", "); }
-			start = 1;
-			printf("%f", va_arg(list, double));
-			break;
-		case's':
-			switch (start)
-			{ case 1: printf(", "); }
-			start = 1;
-			p = va_arg(list, char*);
-			if (p)
-			{ printf("%s", p);
-			break; }
-			printf("%p", p);
-			break; }
-		j++;
+		str = va_arg(strings, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
+
 	printf("\n");
-	va_end(list);
+
+	va_end(strings);
 }
